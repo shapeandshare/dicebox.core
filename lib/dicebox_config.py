01072eed612fa544ad79efb14ca6821b8d7ed1d8
Joshua C. Burt <joshburt@shapeandshare.com>
2017-07-07 16:10:16 -0600

    moved configutation out of library and into file

--- lib/dicebox_config.py
+++ lib/dicebox_config.py
@@ -1,58 +1,83 @@
+import ConfigParser
+import json
+
+my_config = ConfigParser.ConfigParser()
+configFilePath = r'./dicebox.config'
+my_config.read(configFilePath)
+
+
 ###############################################################################
-# Configuration Options
+# Data Set Options
 ###############################################################################
 
-DATASET = 'dicebox'
-NB_CLASSES = 11
-IMAGE_WIDTH=480
-IMAGE_HEIGHT=270
+# Load user defined config
+DATASET = my_config.get('DATASET', 'name')
+NB_CLASSES = my_config.getint('DATASET', 'categories')
+IMAGE_WIDTH = my_config.getint('DATASET', 'image_width')
+IMAGE_HEIGHT = my_config.getint('DATASET', 'image_height')
+DATA_BASE_DIRECTORY = my_config.get('DIRECTORY', 'dataset_base_directory')
+
+# Build Calculated Configs
 NETWORK_NAME = "%s_%ix%i" % (DATASET, IMAGE_WIDTH, IMAGE_HEIGHT)
 INPUT_SHAPE = (IMAGE_WIDTH*IMAGE_HEIGHT,)
-
-DATA_BASE_DIRECTORY = 'datasets'
 DATA_DIRECTORY = "%s/%s/data/" % (DATA_BASE_DIRECTORY, NETWORK_NAME)
 
+
+###############################################################################
+# Neural Network Taxonomy Options
+###############################################################################
 NN_PARAM_CHOICES = {
-    'nb_neurons': [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597],
-    'nb_layers': [1, 2, 3, 5, 8, 13, 21],
-    'activation': ['relu', 'elu', 'tanh', 'sigmoid'],
-    'optimizer': ['rmsprop', 'adam', 'sgd', 'adagrad',
-                  'adadelta', 'adamax', 'nadam']
+    'nb_neurons': json.loads(my_config.get('TAXONOMY', 'neurons')),
+    'nb_layers': json.loads(my_config.get('TAXONOMY', 'layers')),
+    'activation': my_config.get('TAXONOMY', 'activation'),
+    'optimizer': my_config.get('TAXONOMY', 'optimizer')
 }
 
+
+###############################################################################
+# Lonestar Options
 # {'nb_layers': 2, 'activation': 'sigmoid', 'optimizer': 'adamax', 'nb_neurons': 987}
-#07/03/2017 04:02:49 AM - INFO - Network accuracy: 97.27%
+# 07/03/2017 04:02:49 AM - INFO - Network accuracy: 97.27%
+###############################################################################
 NN_LONESTAR_PARAMS = {
-    'nb_neurons': 987,
-    'nb_layers': 2,
-    'activation': 'sigmoid',
-    'optimizer': 'adamax'
+    'nb_neurons': my_config.getint('LONESTAR', 'neurons'),
+    'nb_layers': my_config.getint('LONESTAR', 'layers'),
+    'activation': my_config.get('LONESTAR', 'activation'),
+    'optimizer': my_config.get('LONESTAR', 'optimizer')
 }
 
-EPOCHS = 10000
-GENERATIONS = 100  # Number of times to evole the population.
-POPULATION = 50  # Number of networks in each generation.
-NOISE = 0.1
-
-# Settings for the 1920x1080 dataset
-BATCH_SIZE = 150
-TRAIN_BATCH_SIZE = 1000
-TEST_BATCH_SIZE = 200
-
-# Settings for the 60x50 dataset
-#BATCH_SIZE = 15000
-#TRAIN_BATCH_SIZE = 212000
-#TEST_BATCH_SIZE = 15000
-
-LOGS_DIR='./logs'
-WEIGHTS_DIR='./weights'
-TMP_DIR='./tmp'
 
 ###############################################################################
-# Server Configuration Options
+# Evolution Options
 ###############################################################################
-API_ACCESS_KEY = '6{t}*At&R;kbgl>Mr"K]=F+`EEe'
-API_VERSION = '0.1.0'
-LISTENING_HOST='0.0.0.0'
-FLASK_DEBUG=False
-MODEL_WEIGHTS_FILENAME='weights.best.hdf5'
+EPOCHS = my_config.getint('EVOLUTION', 'epochs')
+GENERATIONS = my_config.getint('EVOLUTION', 'generations')  # Number of times to evole the population.
+POPULATION = my_config.getint('EVOLUTION', 'population')  # Number of networks in each generation.
+NOISE = my_config.getfloat('GLOBAL', 'noise')
+
+
+###############################################################################
+# Training Options / Settings for the 1920x1080 dataset
+###############################################################################
+BATCH_SIZE = my_config.getint('TRAINING', 'batch_size')
+TRAIN_BATCH_SIZE = my_config.getint('TRAINING', 'train_batch_size')
+TEST_BATCH_SIZE = my_config.getint('TRAINING', 'test_batch_size')
+
+
+###############################################################################
+# Direcrtory Options
+###############################################################################
+LOGS_DIR = my_config.get('DIRECTORY', 'logs_dir')
+WEIGHTS_DIR = my_config.get('DIRECTORY', 'weights_dir')
+TMP_DIR = my_config.get('DIRECTORY', 'tmp_dir')
+
+
+###############################################################################
+# Server Options
+###############################################################################
+API_ACCESS_KEY = my_config.get('SERVER', 'api_access_key')
+API_VERSION = my_config.get('SERVER', 'api_version')
+LISTENING_HOST = my_config.get('SERVER', 'listening_host')
+FLASK_DEBUG = my_config.getboolean('SERVER', 'flask_debug')
+MODEL_WEIGHTS_FILENAME = my_config.get('SERVER', 'model_weights_filename')
+
