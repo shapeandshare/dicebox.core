5b55da0c1d4227e5991667a7347253e82d4a676d
Joshua C. Burt <joshburt@shapeandshare.com>
2017-07-08 12:50:57 -0600

    cleanup

--- dicebox_client.py
+++ dicebox_client.py
@@ -23,20 +23,11 @@ ramp_frames = 3
 # Now we can initialize the camera capture object with the cv2.VideoCapture class.
 # All it needs is the index to a camera port.
 camera = cv2.VideoCapture(camera_port)
-
-
 camera.set(cv.CV_CAP_PROP_FRAME_WIDTH, config.IMAGE_WIDTH)
 camera.set(cv.CV_CAP_PROP_FRAME_HEIGHT, config.IMAGE_HEIGHT)
 
-# 780x650
-# camera.set(cv.CV_CAP_PROP_FRAME_WIDTH, 1);
-# camera.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 1);
-
-# 60x50
-# camera.set(cv.CV_CAP_PROP_FRAME_WIDTH, 60);
-# camera.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 50);
-
 font = cv.CV_FONT_HERSHEY_SIMPLEX
+
 
 def get_image():
     retval, im = camera.read()
@@ -49,8 +40,6 @@ def resize_keep_aspect_ratio(input_image, desired_size):
 
     height = float(height)
     width = float(width)
-    # print("height: (%f)" % height)
-    # print("width: (%f)" % width)
 
     if width >= height:
         max_dim = width
@@ -70,14 +59,8 @@ def resize_keep_aspect_ratio(input_image, desired_size):
         new_width = width * scale
         x = (desired_size - new_width) / 2
 
-    # print("desired size: (%f)" % desired_size)
-    # print("x: (%f)" % x)
-    # print("y: (%f)" % y)
-
     new_height = int(math.floor(new_height))
     new_width = int((math.floor(new_width)))
-    # print("new_height: (%i)" % new_height)
-    # print("new_width: (%i)" % new_width)
 
     resized_input = cv2.resize(input_image, (new_width, new_height))
 
@@ -85,8 +68,6 @@ def resize_keep_aspect_ratio(input_image, desired_size):
 
     x_offset = int(math.floor(x+new_width))
     y_offset = int(math.floor(y+new_height))
-    # print("x_offset: (%i)" % x_offset)
-    # print("y_offset: (%i)" % y_offset)
 
     # new lets drop the resized imput onto the output
     output[int(y):int(y_offset), int(x):int(x_offset)] = resized_input
@@ -100,20 +81,12 @@ for i in xrange(ramp_frames):
     temp = get_image()
 
 
-
 with open('./category_map.json') as data_file:
     jdata = json.load(data_file)
 
 server_category_map = {}
 for d in jdata:
-    # print(jdata[d])
-    # print(d)
-    # server_category_map[d] = jdata[d]
-    # print("%s:%s" % (d, jdata[d]))
-    # server_category_map[d] = jdata[d]
     server_category_map[str(jdata[d])] = str(d)
-
-# print(server_category_map)
 
 CURRENT_EXPECTED_CATEGORY_INDEX = 1
 MAX_EXPECTED_CATEGORY_INDEX = len(server_category_map)
@@ -142,8 +115,6 @@ while True:
         file_content = tmp_file.read()
 
     if KEEP_INPUT:
-        # print(MISCLASSIFIED_CATEGORY_INDEX)
-        # print(ONLY_KEEP_MISCLASSIFIED_INPUT)
         if not MISCLASSIFIED_CATEGORY_INDEX and ONLY_KEEP_MISCLASSIFIED_INPUT:
             os.remove(tmp_file_path)
         else:
@@ -176,45 +147,16 @@ while True:
                 if 'prediction' in response.json():
                     prediction = response.json()['prediction']
                     category = server_category_map[str(prediction)]
-                    # print("%s" % prediction)
-                    # print("%s" % category)
     except:
-        # print('.')
         SERVER_ERROR = True
-        # raise
-
-    # print prediction
-
-    # training_category = '1d4_4'
-    # if category == training_category:
-    #     os.rename("./tmp/%s" % filename, "./tmp/%s/%s" % (category, filename))
-    # else:
-    #     print('misclassified')
-    #     os.rename("./tmp/%s" % filename, "./tmp/misclassified/%s" % filename)
-
-    # print(MAX_EXPECTED_CATEGORY_INDEX)
-    # print(CURRENT_EXPECTED_CATEGORY_INDEX)
-    # print(server_category_map[str(CURRENT_EXPECTED_CATEGORY_INDEX-1)])
 
     if category == server_category_map[str(CURRENT_EXPECTED_CATEGORY_INDEX-1)]:
         MISCLASSIFIED_CATEGORY_INDEX = False
-        # print(False)
     else:
         MISCLASSIFIED_CATEGORY_INDEX = True
-        # print(True)
 
     cv2.namedWindow('dice box', cv2.WINDOW_NORMAL)
-    # lets make a pretty output window
-
-    # cv2.putText(camera_capture, "%s" % prediction, (20, 20), font, 0.5, (255, 255, 255), 1)
-    # cv2.imshow('dice box', camera_capture)
-
-    # cv2.putText(camera_capture, "%s" % category, (5, 15), font, 0.5, (255, 255, 255), 1)
-
-    # im[y1:y2, x1:x2]
     output_display = camera_capture
-    # height, width = output_display.shape[:2]
-    # output_display[height-50:height, 0:60] = cv2.cvtColor(resized_image, cv.CV_GRAY2RGB)
 
     output_label_1 = "[pred %s/exp %s][match? %r]" % (category, server_category_map[str(CURRENT_EXPECTED_CATEGORY_INDEX-1)], not MISCLASSIFIED_CATEGORY_INDEX)
     output_label_2 = "[record? %r][only keep misclassified? %r]" % (KEEP_INPUT, ONLY_KEEP_MISCLASSIFIED_INPUT)
