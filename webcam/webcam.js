936434c072e286e5263199b83102f8f8e7274559
Joshua C. Burt <joshburt@shapeandshare.com>
2017-07-30 13:02:06 -0600

    support for converting to grayscale, cropping down to target size, base64 encoding, and creating the outbound json data.

--- webcam/webcam.js
+++ webcam/webcam.js
@@ -2,15 +2,16 @@
     "use strict";
 
     var video, width, height, context;
+    var encoded_data;
 
     function initialize() {
         // The source video.
-        video = doc.getElementById("v");
+        video = doc.getElementById("video");
         width = video.width;
         height = video.height;
 
         // The target canvas.
-        var canvas = doc.getElementById("c");
+        var canvas = doc.getElementById("canvas");
         context = canvas.getContext("2d");
 
         // Get the webcam's stream.
@@ -29,8 +30,19 @@
         var frame = readFrame();
 
         if (frame) {
-            replaceGreen(frame.data);
+            // perform transform on input data
+            // e.g. make gray scale
+            makeGrayScale(frame.data)
             context.putImageData(frame, 0, 0);
+
+            var dataURL = canvas.toDataURL();
+            encoded_data = dataURL.replace(/^data:image\/png;base64\,/, "")
+            //console.log(encoded_data);
+
+            var json_packet={"data":encoded_data};
+            var json_out = JSON.stringify(json_packet);
+            console.log(json_out);
+
         }
 
         // Wait for the next frame.
@@ -39,31 +51,49 @@
 
     function readFrame() {
         try {
-            context.drawImage(video, 0, 0, width, height);
+            var sourceX = width/2 - 50;
+            var sourceY = height/2 - 50;
+
+            //var sourceWidth = width;
+            //var sourceHeight = height;
+            var sourceWidth = 100;
+            var sourceHeight = 100;
+
+            var destWidth = 100;
+            var destHeight = 100;
+            //var destX = width / 2 - destWidth / 2;
+            //var destY = height / 2 - destHeight / 2;
+            var destX = 0;
+            var destY = 0;
+            context.drawImage(video, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+            //context.drawImage(video, width/2 - 50, height/2 - 50, width/2 + 50, height/2 + 50, 0, 0, 100, 100);
         } catch (e) {
             // The video may not be ready, yet.
             return null;
         }
-
-        return context.getImageData(0, 0, width, height);
+        return context.getImageData(0, 0, destWidth, destHeight);
     }
 
-    function replaceGreen(data) {
+    function makeGrayScale(data) {
         var len = data.length;
+        var luma;
 
-        for (var i = 0, j = 0; j < len; i++, j += 4) {
-            // Convert from RGB to HSL...
-            var hsl = rgb2hsl(data[j], data[j + 1], data[j + 2]);
-            var h = hsl[0], s = hsl[1], l = hsl[2];
-
-            // ... and check if we have a somewhat green pixel.
-            if (h >= 90 && h <= 160 && s >= 25 && s <= 90 && l >= 20 && l <= 75) {
-                data[j + 3] = 0;
-            }
+        for (var i = 0; i < len; i += 4) {
+            luma = data[i] * 0.2126 + data[i+1] * 0.7152 + data[i+2] * 0.0722;
+            data[i] = data[i+1] = data[i+2] = luma;
+            //data[i+3] = data[i+3];
+//            // Convert from RGB to HSL...
+//            var hsl = rgb2hsl(data[j], data[j + 1], data[j + 2]);
+//            var h = hsl[0], s = hsl[1], l = hsl[2];
+//
+//            // ... and check if we have a somewhat green pixel.
+//            if (h >= 90 && h <= 160 && s >= 25 && s <= 90 && l >= 20 && l <= 75) {
+//                data[j + 3] = 0;
+//            }
         }
     }
 
-    function rgb2hsl(r, g, b) {
+/*    function rgb2hsl(r, g, b) {
         r /= 255; g /= 255; b /= 255;
 
         var min = Math.min(r, g, b);
@@ -98,7 +128,7 @@
         }
 
         return [h, s * 100, l * 100];
-    }
+    }*/
 
     addEventListener("DOMContentLoaded", initialize);
 })(document, navigator);
