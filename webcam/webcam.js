5e747c39fae2571b98945f2dc879301fa57430eb
Joshua C. Burt <joshburt@shapeandshare.com>
2017-08-03 17:22:48 -0600

    Welp it posts, but barely. :). Need to go deal with CORS server-side.  See you on the other side!

--- webcam/webcam.js
+++ webcam/webcam.js
@@ -41,18 +41,24 @@
 
             var json_packet={"data":encoded_data};
             var json_out = JSON.stringify(json_packet);
-            console.log(json_out);
-/*
-            $.post('http://localhost:5000/api/classify', {
-                'Content-type': 'application/json',
-                'API-ACCESS-KEY': '6e249b5f-b483-4e0d-b50b-81d95e3d9a59',
-                'API-VERSION': '0.2.2',
-                'body': json_out,
-            }, function(serverResponse){
-                console.log(json_out);
-                //do what you want with server response
+            //console.log(json_out);
+
+            $.ajax({
+                url: 'http://localhost:5000/api/classify',
+                type: 'POST',
+                headers:
+                    {
+                        'Content-type': 'application/json',
+                        'API-ACCESS-KEY': '6e249b5f-b483-4e0d-b50b-81d95e3d9a59',
+                        'API-VERSION': '0.2.2'
+                    },
+                data: json_out,
+                success: function(html) {
+                    console.log(html);
+                    //$("#results").append(html);
+                }
             });
-*/
+
         }
 
         // Wait for the next frame.
@@ -102,43 +108,6 @@
 //            }
         }
     }
-
-/*    function rgb2hsl(r, g, b) {
-        r /= 255; g /= 255; b /= 255;
-
-        var min = Math.min(r, g, b);
-        var max = Math.max(r, g, b);
-        var delta = max - min;
-        var h, s, l;
-
-        if (max == min) {
-            h = 0;
-        } else if (r == max) {
-            h = (g - b) / delta;
-        } else if (g == max) {
-            h = 2 + (b - r) / delta;
-        } else if (b == max) {
-            h = 4 + (r - g) / delta;
-        }
-
-        h = Math.min(h * 60, 360);
-
-        if (h < 0) {
-            h += 360;
-        }
-
-        l = (min + max) / 2;
-
-        if (max == min) {
-            s = 0;
-        } else if (l <= 0.5) {
-            s = delta / (max + min);
-        } else {
-            s = delta / (2 - max - min);
-        }
-
-        return [h, s * 100, l * 100];
-    }*/
 
     addEventListener("DOMContentLoaded", initialize);
 })(document, navigator);
