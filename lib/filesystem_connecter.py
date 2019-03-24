8e233782cb351cd31ca0a8e70d63f23c00b7268e
Joshua C. Burt <joshburt@shapeandshare.com>
2019-03-24 16:32:39 -0600

    linting

--- lib/filesystem_connecter.py
+++ lib/filesystem_connecter.py
@@ -1,42 +1,58 @@
+"""Handles filesystem interactions for neural network input data.
+
+###############################################################################
+# Filesystem Connector for Neural Network Input Data
+# Copyright (c) 2017-2019 Joshua Burt
+###############################################################################
+"""
+
+###############################################################################
+# Dependencies
+###############################################################################
 import os
 import fnmatch
 import struct
+import array
+import logging
 import numpy
 from PIL import Image
-from array import *
-import logging
 # from datetime import datetime  # used when dumping raw transforms to disk
-import docker_config as config
+import lib.docker_config as config
 
-class FileSystemConnector:
-
+class FileSystemConnector(object):
+    """File System Connector Class"""
     DATASET_INDEX = None
     DATA_DIRECTORY = None
     CATEGORY_MAP = None
     PIXEL_CACHE = {}
 
     def __init__(self, data_directory, disable_data_indexing=False):
-        if FileSystemConnector.DATA_DIRECTORY is None:
-            FileSystemConnector.DATA_DIRECTORY = data_directory
-            logging.info('data directory: (%s)' % FileSystemConnector.DATA_DIRECTORY)
+        if self.DATA_DIRECTORY is None:
+            self.DATA_DIRECTORY = data_directory
+            logging.info('data directory: (%s)', self.DATA_DIRECTORY)
 
         if disable_data_indexing is False:
-            if FileSystemConnector.DATASET_INDEX is None:
-                FileSystemConnector.DATASET_INDEX = FileSystemConnector.get_data_set(self)
+            if self.DATASET_INDEX is None:
+                self.DATASET_INDEX = self.get_data_set()
                 logging.debug('DATASET_INDEX')
-                logging.debug(FileSystemConnector.DATASET_INDEX)
-    
-            if FileSystemConnector.CATEGORY_MAP is None:
-                FileSystemConnector.CATEGORY_MAP = FileSystemConnector.get_data_set_categories(self)
+                logging.debug(self.DATASET_INDEX)
+
+            if self.CATEGORY_MAP is None:
+                self.CATEGORY_MAP = self.get_data_set_categories()
                 logging.debug('CATEGORY_MAP')
-                logging.debug(FileSystemConnector.CATEGORY_MAP)
+                logging.debug(self.CATEGORY_MAP)
         else:
             logging.info('File System Connector Data Indexing Disabled.')
 
     def get_batch_list(self, batch_size):
+        """For a given batch size, returns a random selection of indices
+
+        :param batch_size: integer value
+        :return: array of indices in the batch size (each index appearing only once).
+        """
         output = []
-        set_size = len(FileSystemConnector.DATASET_INDEX)
-        value_list = FileSystemConnector.DATASET_INDEX.values()
+        set_size = len(self.DATASET_INDEX)
+        value_list = self.DATASET_INDEX.values()
 
         set_indices = []
         for i in range(1, set_size):
@@ -53,17 +69,23 @@ class FileSystemConnector:
         return output
 
     def get_batch(self, batch_size, noise=0):
+        """For a given batch size and noise level, returns a dictionarty of data and labels.
+
+        :param batch_size: integer
+        :param noise: floating point number
+        :return: dictionary of image data and labels
+        """
         image_data = []
         image_labels = []
 
-        category_map = FileSystemConnector.CATEGORY_MAP
+        category_map = self.CATEGORY_MAP
         batch_list = self.get_batch_list(batch_size)
 
         # build file path
         for i in range(0, batch_size):
             item = batch_list[i]
-            filename = "%s%s/%s" % (FileSystemConnector.DATA_DIRECTORY, item[1], item[0])
-            logging.debug("(%s)(%s)" % (category_map[item[1]], filename))
+            filename = "%s%s/%s" % (self.DATA_DIRECTORY, item[1], item[0])
+            logging.debug("(%s)(%s)", category_map[item[1]], filename)
             # logging.info("  natural category label: (%s)" % item[1])
             # logging.info("  neural network category label: (%i)" % category_map[item[1]])
             cat_one_hot = numpy.zeros(len(category_map))
@@ -81,30 +103,32 @@ class FileSystemConnector:
             # [k,v] (filename, pixeldata)
             if self.PIXEL_CACHE.has_key(filename):
                 # found in cache
-                pixel_data = FileSystemConnector.PIXEL_CACHE[filename]
-                logging.debug("loaded cached pixel data for (%s)" % filename)
+                pixel_data = self.PIXEL_CACHE[filename]
+                logging.debug("loaded cached pixel data for (%s)", filename)
             else:
                 pixel_data = self.process_image(filename, noise)
-                FileSystemConnector.PIXEL_CACHE[filename] = pixel_data  # add to cache
-                logging.debug("cached pixel data for (%s)" % filename)
+                self.PIXEL_CACHE[filename] = pixel_data  # add to cache
+                logging.debug("cached pixel data for (%s)", filename)
 
             image_data.append(pixel_data)
         return [image_data, image_labels]
 
-    def lucky(self, noise=0.0):
+    @staticmethod
+    def lucky(noise=0):
         if float(noise) > float(ord(struct.unpack('c', os.urandom(1))[0])) / 255:
             logging.debug('luck bestowed')
             return True
         return False
 
-    def process_image(self, filename, noise=0):
+    @staticmethod
+    def process_image(filename, noise=0):
         pixel_data = array('B')
 
         im = Image.open(filename).convert('L') # Load as gray
 
         original_width, original_height = im.size
         # original_size = original_width, original_height
-        logging.debug("original size: (%i, %i)" % (original_width, original_height))
+        logging.debug("original size: (%i, %i)", original_width, original_height)
 
         # See if there is noise
         if float(noise) > float(ord(struct.unpack('c', os.urandom(1))[0])) / 255:
@@ -112,9 +136,9 @@ class FileSystemConnector:
             rotation_angle = float(ord(struct.unpack('c', os.urandom(1))[0])) / 255 * 10
             if float(ord(struct.unpack('c', os.urandom(1))[0])) / 255 > 0.5:
                 rotation_angle = rotation_angle * -1
-            logging.debug("Rotating image %f" % rotation_angle)
+            logging.debug("Rotating image %f", rotation_angle)
             im = im.rotate(rotation_angle)
-            logging.debug("new size: (%i, %i)" % (im.size[0], im.size[1]))
+            logging.debug("new size: (%i, %i)", im.size[0], im.size[1])
 
         # Perform a random scale
         if float(noise) > float(ord(struct.unpack('c', os.urandom(1))[0])) / 255:
@@ -123,7 +147,7 @@ class FileSystemConnector:
             while random_scale < 0.7:
                 random_scale = 1.5 * float(ord(struct.unpack('c', os.urandom(1))[0])) / 255
 
-            logging.debug("Scaling image %f" % random_scale)
+            logging.debug("Scaling image %f", random_scale)
             width, height = im.size
             new_width = int(width * random_scale)
             new_height = int(height * random_scale)
@@ -132,7 +156,7 @@ class FileSystemConnector:
                 # logging.info(new_size)
                 im = im.resize(new_size)
                 # im = im.resize(new_size, Image.ANTIALIAS)
-                logging.debug("new size: (%i, %i)" % (im.size[0], im.size[1]))
+                logging.debug("new size: (%i, %i)", im.size[0], im.size[1])
 
         # Crop Image If Required
         logging.debug('Crop image if required')
@@ -146,18 +170,18 @@ class FileSystemConnector:
         upper = int(new_middle_y - float(original_height) / 2)
         right = int(new_middle_x + float(original_width) / 2)
         lower = int(new_middle_y + float(original_height) / 2)
-        logging.debug("left: %i" % left)
-        logging.debug("upper: %i" % upper)
-        logging.debug("right: %i" % right)
-        logging.debug("lower: %i" % lower)
+        logging.debug("left: %i", left)
+        logging.debug("upper: %i", upper)
+        logging.debug("right: %i", right)
+        logging.debug("lower: %i", lower)
 
         im = im.crop((left, upper, right, lower))
-        logging.debug("new size: (%i, %i)" % (im.size[0], im.size[1]))
+        logging.debug("new size: (%i, %i)", im.size[0], im.size[1])
 
         # Ensure the input will match in input tensor
         #im = im.resize((original_width, original_height), Image.ANTIALIAS)
         im = im.resize((config.IMAGE_WIDTH, config.IMAGE_HEIGHT), Image.ANTIALIAS)
-        logging.debug("new size: (%i, %i)" % (im.size[0], im.size[1]))
+        logging.debug("new size: (%i, %i)", im.size[0], im.size[1])
 
         # dump to file for manual review
         # filename = datetime.now().strftime('transform_%Y-%m-%d_%H_%M_%S_%f.png')
@@ -181,11 +205,10 @@ class FileSystemConnector:
         data = numpy.frombuffer(pixel_data, dtype=numpy.uint8)
         return data
 
-
     def get_data_set_categories(self):
         natural_categories = []
         category_map = {}
-        value_list = FileSystemConnector.DATASET_INDEX.values()
+        value_list = self.DATASET_INDEX.values()
         for item in value_list:
             # logging.info(item)
             # logging.info("natural category label: (%s)" % item[1])
@@ -200,16 +223,13 @@ class FileSystemConnector:
         return category_map
 
     def get_data_set(self):
-        # data_directory = '/home/console/Workbench/Repositories/dicebox/train/data/'
-        # data_directory = FileSystemConnector.DATA_DIRECTORY
         data_set = {}
-        for root, dirnames, filenames in os.walk(FileSystemConnector.DATA_DIRECTORY):
+        for root, dirnames, filenames in os.walk(self.DATA_DIRECTORY):
             for filename in fnmatch.filter(filenames, '*.png'):
                 new_entry = str(os.path.join(root, filename))
-                new_entry = new_entry.replace(FileSystemConnector.DATA_DIRECTORY, '')
+                new_entry = new_entry.replace(self.DATA_DIRECTORY, '')
                 category, filename = new_entry.split('/')
                 # logging.info("category: (%s), filename: (%s)" % (category, filename))
                 data_set[new_entry] = [filename, category]
         # logging.info(data_set)
         return data_set
-
