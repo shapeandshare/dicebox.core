05c712668e06ff93b86ec4408add4caec1416800
Joshua C. Burt <joshburt@shapeandshare.com>
2019-03-30 11:00:55 -0600

    more tests!

--- test/test_filesystem_connector.py
+++ test/test_filesystem_connector.py
@@ -21,6 +21,8 @@ class Test(unittest.TestCase):
     The basic class that inherits unittest.TestCase
     """
 
+    EXPECTED_DATASET_INDEX = None
+    EXPECTED_CATEGORY_MAP = None
     DATASET_LOCATION = 'test/data/test_dataset/data'
     DISABLE_DATA_INDEXING = False
 
@@ -28,29 +30,41 @@ class Test(unittest.TestCase):
         # instantiate the file system connector Class
         self.fsc = filesystemconnectorclass.FileSystemConnector(self.DATASET_LOCATION, self.DISABLE_DATA_INDEXING)
 
+        with open('test/data/DATASET_INDEX.json') as json_file:
+            self.EXPECTED_DATASET_INDEX = json.load(json_file)
+        if self.EXPECTED_DATASET_INDEX is None:
+            Exception('Unable to load data/DATASET_INDEX.json!')
+
+        with open('test/data/CATEGORY_MAP.json') as json_file:
+            self.EXPECTED_CATEGORY_MAP = json.load(json_file)
+        if self.EXPECTED_CATEGORY_MAP is None:
+            Exception('Unable to load data/CATEGORY_MAP.json!')
+
     def test_class_variable_DATA_DIRECTORY(self):
         self.assertEqual(self.DATASET_LOCATION, self.fsc.DATA_DIRECTORY)
 
     def test_class_variable_DATASET_INDEX(self):
-        expected_dataset_index = None
-        with open('test/data/DATASET_INDEX.json') as json_file:
-            expected_dataset_index = json.load(json_file)
-        if expected_dataset_index is None:
-            Exception('Unable to load data/DATASET_INDEX.json!')
-
-        self.assertEqual(expected_dataset_index, self.fsc.DATASET_INDEX)
+        self.assertEqual(self.EXPECTED_DATASET_INDEX, self.fsc.DATASET_INDEX)
 
     def test_class_variable_CATEGORY_MAP(self):
-        expected_category_map = None
-        with open('test/data/CATEGORY_MAP.json') as json_file:
-            expected_category_map = json.load(json_file)
-        if expected_category_map is None:
-            Exception('Unable to load data/CATEGORY_MAP.json!')
+        self.assertEqual(self.EXPECTED_CATEGORY_MAP, self.fsc.CATEGORY_MAP)
 
-        self.assertEqual(expected_category_map, self.fsc.CATEGORY_MAP)
+    def test_get_batch_list(self):
+        batch_size = 0
+        expected_batch = []
+        returned_batch = self.fsc.get_batch_list(batch_size)
+        self.assertEqual(expected_batch, returned_batch)
 
-
-
+        batch_size = 1
+        returned_batch = self.fsc.get_batch_list(batch_size)
+        logging.debug(returned_batch[0])
+        item = returned_batch[0]
+        found = False
+        for value in self.EXPECTED_DATASET_INDEX.values():
+            if value == item:
+                found = True
+                break
+        self.assertTrue(found)
 
 if __name__ == '__main__':
     # begin the unittest.main()
