3f732ce4928a552fcd6d824b094ce6d0827203d4
Joshua C. Burt <joshburt@shapeandshare.com>
2019-04-07 17:27:49 -0600

    some more random helpers

--- dicebox/utils/helpers.py
+++ dicebox/utils/helpers.py
@@ -3,6 +3,7 @@ import os
 import struct
 import math
 
+
 def lucky(noise=0):
     """Simple function to determine whether or not a thing occurs.
 
@@ -19,3 +20,17 @@ def random_index(set_size):
     rand = float(ord(struct.unpack('c', os.urandom(1))[0])) / 255
     return int(math.ceil(rand * set_size))
 
+
+def random_index_between(min_index=0, max_index=1):
+    if min_index > max_index:
+        raise Exception('max must be greater than or equal to the min')
+
+    rand = float(ord(struct.unpack('c', os.urandom(1))[0])) / 255
+    delta = max_index - min_index
+    delta_offset = int(math.ceil(rand * delta))
+    final_index = min_index + delta_offset
+    return final_index
+
+
+def random():
+    return float(ord(struct.unpack('c', os.urandom(1))[0])) / 255
