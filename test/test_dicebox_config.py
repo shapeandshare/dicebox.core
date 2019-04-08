378af4f5630faa00ee7ac8859172cca21f7b7380
Joshua C. Burt <joshburt@shapeandshare.com>
2019-04-07 18:26:57 -0600

    remove duplicate logger

--- test/test_dicebox_config.py
+++ test/test_dicebox_config.py
@@ -7,15 +7,6 @@ import numpy.testing
 from dicebox.config.dicebox_config import DiceboxConfig
 
 
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
-
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-root.addHandler(handler)
-
 class Test(unittest.TestCase):
     """
     The basic class that inherits unittest.TestCase
