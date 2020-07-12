ec687b964c60e5ceee8efbda54f2ed89f0819f64
Josh Burt <joshburt@shapeandshare.com>
2020-07-12 16:45:38 -0600

    if logger output

--- test/unit/PrimordialPoolTest.py
+++ test/unit/PrimordialPoolTest.py
@@ -6,6 +6,17 @@ from src.shapeandshare.dicebox import PrimordialPool
 from src.shapeandshare.dicebox.config import DiceboxConfig
 from src.shapeandshare.dicebox.utils import make_sure_path_exists
 
+###############################################################################
+# Setup logging.
+###############################################################################
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=logging.INFO,
+    filemode='w',
+    filename="primordialpool.log"
+)
+
 
 class PrimordialPoolTest(unittest.TestCase):
 
@@ -13,17 +24,6 @@ class PrimordialPoolTest(unittest.TestCase):
         config_file = 'test/fixtures/mnist/dicebox.config'
         dicebox_config: DiceboxConfig = DiceboxConfig(config_file=config_file)
 
-        ###############################################################################
-        # Setup logging.
-        ###############################################################################
-        make_sure_path_exists(dicebox_config.LOGS_DIR)
-        logging.basicConfig(
-            format='%(asctime)s - %(levelname)s - %(message)s',
-            datefmt='%m/%d/%Y %I:%M:%S %p',
-            level=logging.INFO,
-            filemode='w',
-            filename="%s/primordialpool.%s.log" % (dicebox_config.LOGS_DIR, os.uname()[1])
-        )
         logging.info("***Evolving %d generations with population %d***" % (dicebox_config.GENERATIONS, dicebox_config.POPULATION))
         pool: PrimordialPool = PrimordialPool(config=dicebox_config)
         pool.generate()
